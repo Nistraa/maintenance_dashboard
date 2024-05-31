aaff0ff925757c8abc58963a0c75003f0221e6aa
Nistraa <nikola.strika@protonmail.com>
2024-05-31 02:11:29 +0200

    REFACTOR: refactored typing of methods in PreProcessDataService; wrote docs for methods in PreProcessDataService

--- services/preprocess_data.py
+++ services/preprocess_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, RobustScaler
 from category_encoders import TargetEncoder
+
+
 # Service to preprocess data
 class PreprocessDataService:
     def __init__(self) -> None:
@@ -10,37 +12,61 @@ class PreprocessDataService:
         def __init__(self) -> None:
             pass
 
-        def forward_fill_data(self, data: pd.DataFrame|pd.Series) -> pd.DataFrame|pd.Series:
+        '''
+        Fills missing data with precedent values from sequence
+        '''
+        def forward_fill_data(self, data: pd.DataFrame) -> pd.DataFrame:
             return data.ffill()
         
-        def backward_fill_data(self, data: pd.DataFrame|pd.Series) -> pd.DataFrame|pd.Series:
+        '''
+        Fills missing data with antecedent values from sequence
+        '''
+        def backward_fill_data(self, data: pd.DataFrame) -> pd.DataFrame:
             return data.bfill()
         
-        def imputate_mean_for_missing_data(self, data: pd.DataFrame|pd.Series) -> pd.DataFrame|pd.Series:
+        '''
+        Fills missing data with mean of sequence
+        '''
+        def imputate_mean_for_missing_data(self, data: pd.DataFrame) -> pd.DataFrame:
             return data.fillna(data.mean())
-        
-        def imputate_median_for_missing_data(self, data: pd.DataFrame|pd.Series) -> pd.DataFrame|pd.Series:
+        '''
+        Fills missing data with middle value
+        '''
+        def imputate_median_for_missing_data(self, data: pd.DataFrame) -> pd.DataFrame:
             return data.fillna(data.median())
-        
-        def imputate_mode_for_missing_data(self, data: pd.DataFrame|pd.Series) -> pd.DataFrame|pd.Series:
+        '''
+        Fills missing data with most occuring value
+        '''
+        def imputate_mode_for_missing_data(self, data: pd.DataFrame) -> pd.DataFrame:
             return data.fillna(data.mode().iloc[0])
-                
-        def interpolate_linear_missing_data(self, data: pd.DataFrame|pd.Series) -> pd.DataFrame|pd.Series:
+        '''
+        Interpolates missing data by estimating within a range of data points
+        '''     
+        def interpolate_linear_missing_data(self, data: pd.Series) -> pd.Series:
             return data.interpolate(method='linear')
-        
-        def interpolate_knn_missing_data(self, data: pd.DataFrame|pd.Series, neighbors: int = 5) -> pd.Series:
+        '''
+        Interpolates missing data based on its nearest neighbours
+        '''
+        def interpolate_knn_missing_data(self, data: pd.DataFrame, neighbors: int = 5) -> pd.Series:
             return pd.Series([data_point[0] for data_point in KNNImputer(n_neighbors=neighbors).fit_transform(data)])
         
     class HandleCategoricalVariables:
         def __init__(self) -> None:
             pass
 
+        '''
+        Converts categories to unique integers
+        '''
         def label_encode_variables(self, data: pd.Series) -> pd.Series:
             return LabelEncoder().fit_transform(data)
-        
-        def one_hot_encode_variables(self, data: pd.DataFrame|pd.Series, columns) -> pd.DataFrame|pd.Series:
+        '''
+        Converts categories to binary vectors
+        '''
+        def one_hot_encode_variables(self, data: pd.Series, columns) -> pd.Series:
             return pd.get_dummies(data, columns)
-        
+        '''
+        Converts categories to the mean of the target variable
+        '''
         def target_encode_variables(self, category: pd.Series, target: pd.Series) -> pd.DataFrame:
             return TargetEncoder().fit_transform(category, target)
         
@@ -49,8 +75,12 @@ class PreprocessDataService:
         def __init__(self) -> None:
             pass
 
-        def min_max_scale_features(self, data: pd.DataFrame|pd.Series) -> pd.DataFrame|pd.Series:
+        '''
+        Scales data within in a specifeid range
+        '''
+        def min_max_scale_features(self, data: pd.Series) -> pd.Series:
             return MinMaxScaler().fit_transform(data)
-    
-        def robust_scale_features(self, data: pd.DataFrame|pd.Series) -> pd.DataFrame|pd.Series:
+        '''
+        Scales data robust to outliers'''
+        def robust_scale_features(self, data: pd.Series) -> pd.Series:
             return RobustScaler().fit_transform(data)
