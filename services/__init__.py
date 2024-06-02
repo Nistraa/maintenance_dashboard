dd82058bcf9d2f36b7c0a6bb52c44578190f39ed
Nistraa <nikola.strika@protonmail.com>
2024-06-02 15:58:28 +0200

    REFACTOR: ensured functionality of all tests

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,5 @@
 from .extract_features import ExtractFeaturesService
 from .load_data import LoadDataService
-from .predict_failures import PredictFailuresService
 from .preprocess_data import PreprocessDataService
 from .visualize_data import VisualizeDataService
 from .model_service import ModelService
